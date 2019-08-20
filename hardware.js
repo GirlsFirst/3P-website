487785060567494332b7d5b4d6c32f01060d2df2
Julia Rodano <juliarodano@Julias-MacBook-Pro-3.local>
2019-08-20 10:59:14 -0400

    push

--- hardware.js
+++ hardware.js
@@ -2,12 +2,12 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgb(0, 0, 0)";
+  document.body.style.backgroundColor = "rgb(45, 68, 79)";
 
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
-  document.body.style.backgroundColor = "black"; // color of background when not closed
+  document.body.style.backgroundColor = "rgb(45, 68, 79)"; // color of background when not closed
 }
