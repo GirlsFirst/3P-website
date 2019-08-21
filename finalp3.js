75c585ff84876d9daccd241054aeed0a3c155aa2
Liz0506 <lizbeth30058@gmail.com>
2019-08-21 11:20:05 -0400

    old

--- finalp3.js
+++ finalp3.js
@@ -7,20 +7,14 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-<<<<<<< HEAD
   document.body.style.backgroundColor = "rgb(45, 68, 79)";
-=======
-  document.body.style.backgroundColor = "rgb(45,68,79)";
->>>>>>> f84cdd6f2b53880ddd75754510ed1b2ce96aeb75
+
 
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
-<<<<<<< HEAD
   document.body.style.backgroundColor = "rgb(45, 68, 79)"; // color of background when not closed
-=======
-  document.body.style.backgroundColor = "rgb(45,68,79)"; // color of background when not closed
->>>>>>> f84cdd6f2b53880ddd75754510ed1b2ce96aeb75
+
 }
