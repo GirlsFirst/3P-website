89592901938c07d8fb30503368cd5f548f5c47fd
Julia Rodano <juliarodano@Julias-MacBook-Pro-3.local>
2019-08-21 11:27:55 -0400

    uiyg

--- finalp3.js
+++ finalp3.js
@@ -8,10 +8,7 @@ function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgb(45, 68, 79)";
-<<<<<<< HEAD
 
-=======
->>>>>>> bafdb282714971903990fa91ff114261086cbf75
 
 }
 
@@ -19,8 +16,5 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "rgb(45, 68, 79)"; // color of background when not closed
-<<<<<<< HEAD
 
-=======
->>>>>>> bafdb282714971903990fa91ff114261086cbf75
 }
